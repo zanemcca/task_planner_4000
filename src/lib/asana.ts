613f49d6ea08ac6fbb6402149b9c3e28cddfea7f
Zane McCaig <zanemccaig@gmail.com>
2020-02-07 18:05:44 -0800

    Trying to fetch the url instead of pushing it

--- src/lib/asana.ts
+++ src/lib/asana.ts
@@ -2,8 +2,8 @@
 import Asana from 'asana';
 
 const ASANA_CLIENT_ID = `1160872085336118`
-const ASANA_REDIRECT_URI = process.env.NODE_ENV === 'production' ? 'https://tp4000.zanemccaig.com' :
-  process.env.NODE_ENV === 'test' ? 'https://tp4000preview.zanemccaig.com' : 'http://localhost:3000'
+
+const ASANA_REDIRECT_URI = process.env.NODE_ENV === 'production' ? window.location.href : 'http://localhost:3000'
 
 export const client = Asana.Client.create({
   clientId: ASANA_CLIENT_ID,
