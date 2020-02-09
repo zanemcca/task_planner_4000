82adced7233ae5975be90e6fe5e188c2d133b32c
Zane McCaig <zanemccaig@gmail.com>
2020-02-08 20:37:43 -0800

    Added some more channels

--- src/hooks/channels.tsx
+++ src/hooks/channels.tsx
@@ -2,10 +2,22 @@
 export const useChannels = () => ({
   default: {
     title: 'default',
-    color: 'red'
+    color: '#36E8FF'
+  },
+  backlog: {
+    title: 'backlog',
+    color: '#FFD982'
+  },
+  critical: {
+    title: 'critical',
+    color: '#26547C'
+  },
+  important: {
+    title: 'important',
+    color: '#EF476F'
   },
   personal: {
     title: 'personal',
-    color: 'blue'
+    color: '#FCA778'
   }
 })
